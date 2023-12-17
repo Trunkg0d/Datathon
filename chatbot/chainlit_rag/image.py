696adfaebc5f2de8fbf4ff1c7bb5df62ed4f4b15
nghia3anp3 <bapngan892003@gmail.com>
2023-12-17 11:07:17 +0700

    Upload chatbot

---
+++ chatbot/chainlit_rag/image.py
@@ -0,0 +1,16 @@
+image_dict = {
+    'description':['a jean with black color, can be hot and not suitable for summer'
+                   ,'a nike black shirt, good for wearing with a white jeans'
+                   ,'a caro nike shirt, really suitable for wearing to a party with friend or crush'
+                   , 'a full black nike suit, it could make you become fashion and fit with some in black concept'
+                   ,'a green nike shirt, better for wearing at home, not good for go to party, a beach party is reccommended'
+                   , 'a wave pattern nike shirt, it is fashion, so you can wear it to some fashion show'],
+    'path': [
+        'E:\chatbot\chainlit_rag\image\nike_black_jeans.jpeg',
+        'E:\chatbot\chainlit_rag\image\nike_black_shirt.png',
+        'E:\chatbot\chainlit_rag\image\nike_caro.png',
+        'E:\chatbot\chainlit_rag\image\nike_full_black.png',
+        'E:\chatbot\chainlit_rag\image\nike_green_shirt.png',
+        'E:\chatbot\chainlit_rag\image\nike_wave.png'
+    ]
+}
